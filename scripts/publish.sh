1f7aaeecf10a050d4e2c5854b4e94a28a6d1fe2a
John Newbigin <john.newbigin@rea-group.com>
2020-03-01 22:45:53 +1100

    Update publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Once all is working, run:
+# $ scripts/publish.sh upload.pypi.org
+
 rm -f dist/*
 python3 setup.py sdist bdist_wheel
-python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+
+SERVER="${1:-test.pypi.org}"
+python3 -m twine upload --repository-url "https://$SERVER/legacy/" dist/*
