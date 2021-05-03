a752a250de16c31256a48387dcee46fbbaf7ae41
John Newbigin <john.newbigin@rea-group.com>
2021-05-03 13:17:45 +1000

    fix up the code for linting and type checking

--- pkcs7_detached/__init__.py
+++ pkcs7_detached/__init__.py
@@ -1,9 +1,5 @@
 # vim: set tabstop=4 shiftwidth=4 expandtab:
 
-import logging
-
-from cryptography.hazmat.backends.openssl.backend import backend
-from cryptography.hazmat.bindings._openssl import ffi, lib
 
 """
 Use the Cryprography hazmat backend (OpenSSL binding) to verify
@@ -14,57 +10,85 @@ Base on the steps in https://github.com/openssl/openssl/blob/master/apps/smime.c
 certificate is a PEM formated X509 certificate
 """
 
+import logging
 
-def verify_detached_signature(document, signature, certificate):
+from cryptography.hazmat.backends.openssl.backend import backend # type: ignore
+from cryptography.hazmat.bindings._openssl import ffi, lib # type: ignore # pylint: disable=E0611
+
+class ValidationError(Exception):
+    """Represent an error in the PKCS7 detached signature validation"""
+
+def __ascii_armor(payload: str, header: str) -> str:
+    """Add ascii armour for the specified header type to an ascii payload
+    Will not add if the correct header is already present
+    Will remove any trailing newlines
+    """
+    try:
+        begin = "-----BEGIN {}-----".format(header)
+        end = "-----END {}-----".format(header)
+
+        lines = payload.split("\n")
+
+        # Remove blank training lines
+        while lines[-1] == "":
+            lines.pop()
+
+        # Add armour if missing
+        if not lines[0] == begin:
+            lines.insert(0, begin)
+            lines.append(end)
+
+        return "\n".join(lines)
+    except IndexError:
+        raise ValidationError(f"Unable to ascii armor {header}")
+
+
+def verify_detached_signature(document: str, signature: str, certificate: str) -> bool:
+    """Verify a detached signature
+    Parameters
+    ----------
+    document : str
+        The document to be verified
+    signature : str
+        The signature to verify. The signature may inculde the PKCS7 ascii armor
+    certificate : str
+        The certificate of the signer. The certificate must include the CERTIFICATE ascii armor
+    """
     logger = logging.getLogger(__name__)
 
     logger.debug(
-        "verify_detached_signature('{}', '{}', '{}')".format(
-            document, signature, certificate
-        )
+        "verify_detached_signature('%s', '%s', '%s')", document, signature, certificate
     )
 
     try:
-        # Format the signature into PKCS7 PEM format
-        pkcs7 = signature.split("\n")
-
-        # Remove blank training lines
-        while pkcs7[-1] == "":
-            pkcs7.pop()
-
-        # Add armour if missing
-        if not pkcs7[0] == "-----BEGIN PKCS7-----":
-            pkcs7.insert(0, "-----BEGIN PKCS7-----")
-            pkcs7.append("-----END PKCS7-----")
-
-        pkcs7 = "\n".join(pkcs7)
-
+        pkcs7 = __ascii_armor(signature, "PKCS7")
         logger.debug(pkcs7)
 
         # Load the string into a bio
-        bio = backend._bytes_to_bio(pkcs7.encode())
+        bio = backend._bytes_to_bio(pkcs7.encode()) # pylint: disable=W0212
 
         # Create the pkcs7 object
-        p7 = lib.PEM_read_bio_PKCS7(bio.bio, ffi.NULL, ffi.NULL, ffi.NULL)
+        pkcs7_object = lib.PEM_read_bio_PKCS7(bio.bio, ffi.NULL, ffi.NULL, ffi.NULL)
 
         # Load the specified certificate
+        certificate = __ascii_armor(certificate, 'CERTIFICATE')
         other_cert = backend.load_pem_x509_certificate(certificate.encode())
         stack = lib.sk_X509_new_null()
-        lib.sk_X509_push(stack, other_cert._x509)
+        lib.sk_X509_push(stack, other_cert._x509) # pylint: disable=W0212
 
         # We need a CA store, even though we don't use it
         store = lib.X509_STORE_new()
 
         # Load the document into a bio
-        content = backend._bytes_to_bio(document.encode("utf-8"))
+        content = backend._bytes_to_bio(document.encode("utf-8")) # pylint: disable=W0212
 
         # Flags
         flags = lib.PKCS7_NOVERIFY
 
         # https://www.openssl.org/docs/man1.0.2/crypto/PKCS7_verify.html
-        if lib.PKCS7_verify(p7, stack, store, content.bio, ffi.NULL, flags) == 1:
+        if lib.PKCS7_verify(pkcs7_object, stack, store, content.bio, ffi.NULL, flags) == 1:
             return True
-    except Exception as e:
-        logger.error("verify_detached_signature exception={}".format(str(e)))
+    except ValidationError as error:
+        logger.error("verify_detached_signature exception=%s", str(error))
         return False
     return False
